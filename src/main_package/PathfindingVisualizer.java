f3d1d18d6fbdfb3d6cc4a0c7c5ad953914fa2298
code2468 <navpreet246kaur@gmail.com>
2021-01-28 00:29:24 +0530

    main package added

---
+++ src/main_package/PathfindingVisualizer.java
@@ -0,0 +1,15 @@
+package main_package;
+
+import model_package.PathMap;
+import view_package.Window;
+
+public class PathfindingVisualizer {
+
+    public static void main(String[] args){
+
+        PathMap map = new PathMap(40, 40);
+        Window window = new Window(map);
+
+    }
+
+}
